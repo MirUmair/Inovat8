07f7964d1ecebe1532a6c36bc2698f23934ba917
ehteshamirshad <156424451+ehteshamirshad@users.noreply.github.com>
2024-10-07 12:19:15 +0500

    Navbar content added

--- src/pages/home/HeaderPage.js
+++ src/pages/home/HeaderPage.js
@@ -1,70 +1,72 @@
-import React, { useState, useEffect, useRef, useMemo } from 'react';
-import '../../styles/HeaderPage.css'; // Import your CSS file
-
-import Navbar from '../../components/Navbar'; // Adjusted import path
-import image from '../../assets/images';
+import React, { useState, useEffect, useRef, useMemo } from "react";
+import "../../styles/HeaderPage.css"; 
+import Navbar from "../../components/Navbar"; 
+import image from "../../assets/images";
 const Header = () => {
-    const texts = [
-        'Shaping the Future with Technology & Innovation.',
-        'Driving Business Excellence with Scalable Digital Solutions',
-        'Extract, Connect & Make Sense of Complex Data.',
-        'Simplify the Dev Journey, Maximize Efficiency',
-    ];
-    const text2 = [
-        '',
-        'AI/ML is at the core of everything we do.',
-        'Boost competitiveness with AI integration.',
-        'Innovate with MENDIX: 10X faster enterprise solutions',
-    ];
+  const texts = [
+    "Shaping the Future with Technology & Innovation.",
+    "Driving Business Excellence with Scalable Digital Solutions",
+    "Extract, Connect & Make Sense of Complex Data.",
+    "Simplify the Dev Journey, Maximize Efficiency",
+  ];
+  const text2 = [
+    "",
+    "AI/ML is at the core of everything we do.",
+    "Boost competitiveness with AI integration.",
+    "Innovate with MENDIX: 10X faster enterprise solutions",
+  ];
 
-    const images = useMemo(() => [image.lightbulb1, '', image.lightbulb3, image.lightbulb4], [image]);
-    // State to track current text index and animation state
-    const [currentText, setCurrentText] = useState(0);
-    const [isAnimating, setIsAnimating] = useState(false);
-    const textRef = useRef(null);
+  const images = useMemo(
+    () => [image.lightbulb1, "", image.lightbulb3, image.lightbulb4],
+    [image]
+  );
+  // State to track current text index and animation state
+  const [currentText, setCurrentText] = useState(0);
+  const [isAnimating, setIsAnimating] = useState(false);
+  const textRef = useRef(null);
 
-    const hideDuration = 1000; // Duration for hiding the text
-    const showDuration = 4000; // Duration to show the text (before hiding again)
-    useEffect(() => {
-        const interval = setInterval(() => {
-            // Trigger hide animation first
-            setIsAnimating(false);
-            // Update the text and image only after hide animation is done
-            setTimeout(() => {
-                setCurrentText((prevTextIndex) => (prevTextIndex + 1) % texts.length);
-                // Trigger show animation
-                setIsAnimating(true);
-            }, hideDuration); // Wait until the hide animation is done before showing the new text
-
-        }, showDuration + hideDuration); // Adjust interval timing to fit both hide and show durations
-        // Cleanup the interval on component unmount
-        return () => clearInterval(interval);
-    }, []);
-    return (
-        <div className="App">
-            <Navbar />
-            <div className="content">
-                <div className="contentText">
-                    <p className={`text ${isAnimating ? 'show' : 'hide'}`} ref={textRef}>
-                        {texts[currentText].split(' ').slice(0, 3).join(' ')}<br />
-                        {texts[currentText].split(' ').slice(3).join(' ')}
-                    </p>
-                    <p className='textbottom'>
-                    {text2[currentText]}
-                    </p>
-                </div>
-                {images[currentText] && <img src={images[currentText]} alt="Lighting bulb" className="image" />}
-            </div>
-            <div className="textcontent">
-                <p className='textbottom'>
-                    Let's get
-                </p>
-                <p className='textbottom1'>
-                    innovative &gt;
-                </p>
-            </div>
+  const hideDuration = 1000; // Duration for hiding the text
+  const showDuration = 4000; // Duration to show the text (before hiding again)
+  useEffect(() => {
+    const interval = setInterval(() => {
+      // Trigger hide animation first
+      setIsAnimating(false);
+      // Update the text and image only after hide animation is done
+      setTimeout(() => {
+        setCurrentText((prevTextIndex) => (prevTextIndex + 1) % texts.length);
+        // Trigger show animation
+        setIsAnimating(true);
+      }, hideDuration); // Wait until the hide animation is done before showing the new text
+    }, showDuration + hideDuration); // Adjust interval timing to fit both hide and show durations
+    // Cleanup the interval on component unmount
+    return () => clearInterval(interval);
+  }, []);
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="content">
+        <div className="contentText">
+          <p className={`text ${isAnimating ? "show" : "hide"}`} ref={textRef}>
+            {texts[currentText].split(" ").slice(0, 3).join(" ")}
+            <br />
+            {texts[currentText].split(" ").slice(3).join(" ")}
+          </p>
+          <p className="textbottom">{text2[currentText]}</p>
         </div>
-    );
+        {images[currentText] && (
+          <img
+            src={images[currentText]}
+            alt="Lighting bulb"
+            className="image"
+          />
+        )}
+      </div>
+      <div className="textcontent">
+        <p className="textbottom">Let's get</p>
+        <p className="textbottom1">innovative &gt;</p>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
--- src/pages/home/MiddleHeader.js
+++ src/pages/home/MiddleHeader.js
@@ -1,130 +1,198 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { useInView } from 'react-intersection-observer';
-import '../../styles/MiddleHeader.css'; // Import your CSS file
-import "slick-carousel/slick/slick.css"; // Import the core slick carousel styles
+import React, { useState, useEffect, useRef } from "react";
+import { useInView } from "react-intersection-observer";
+import "../../styles/MiddleHeader.css";
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import images from '../../assets/images';
+import images from "../../assets/images";
 
 const HeaderPage = () => {
-   
-    const [currentIndex, setCurrentIndex] = useState(0);
-    const sliderRef = useRef(null);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const sliderRef = useRef(null);
 
-    const slides = [
-        {
-            title: 'AI/ML',
-            description: 'Enhance the capabilities of AI/ML with ease through Inov8, your indispensable partner in accelerating the development and deployment of cutting-edge AI/ML solutions. Inov8 empowers businesses to spearhead technological progress by enabling quick adaptations and innovations to navigate the ever-evolving AI/ML domain proficiently.'
-        },
-        {
-            title: 'Development',
-            description: 'Our expertise lies in developing enterprise-level applications utilizing low code approaches. Our team helps streamline development with transparency, agility, and a commitment to your goals.'
-        },
-        {
-            title: 'AI/Enhance',
-            description: 'Enhance the capabilities of AI/ML with ease through Inov8, your indispensable partner in accelerating the development and deployment of cutting-edge AI/ML solutions. Inov8 empowers businesses to spearhead technological progress by enabling quick adaptations and innovations to navigate the ever-evolving AI/ML domain proficiently.'
-        },
-        {
-            title: 'Development Enhance',
-            description: 'Our expertise lies in developing enterprise-level applications utilizing low code approaches. Our team helps streamline development with transparency, agility, and a commitment to your goals.'
-        }
-    ];
+  const slides = [
+    {
+      title: "AI/ML",
+      description:
+        "Enhance the capabilities of AI/ML with ease through Inov8, your indispensable partner in accelerating the development and deployment of cutting-edge AI/ML solutions. Inov8 empowers businesses to spearhead technological progress by enabling quick adaptations and innovations to navigate the ever-evolving AI/ML domain proficiently.",
+    },
+    {
+      title: "Development",
+      description:
+        "Our expertise lies in developing enterprise-level applications utilizing low code approaches. Our team helps streamline development with transparency, agility, and a commitment to your goals.",
+    },
+    {
+      title: "AI/Enhance",
+      description:
+        "Enhance the capabilities of AI/ML with ease through Inov8, your indispensable partner in accelerating the development and deployment of cutting-edge AI/ML solutions. Inov8 empowers businesses to spearhead technological progress by enabling quick adaptations and innovations to navigate the ever-evolving AI/ML domain proficiently.",
+    },
+    {
+      title: "Development Enhance",
+      description:
+        "Our expertise lies in developing enterprise-level applications utilizing low code approaches. Our team helps streamline development with transparency, agility, and a commitment to your goals.",
+    },
+  ];
+  const updateSliderPosition = () => {
+    if (sliderRef.current) {
+      const slideWidth = sliderRef.current.clientWidth;
+      sliderRef.current.style.transform = `translateX(-${
+        currentIndex * slideWidth
+      }px)`;
+    }
+  };
+  useEffect(() => {
+    updateSliderPosition();
+  }, [currentIndex]);
 
-    const updateSliderPosition = () => {
-        if (sliderRef.current) {
-            const slideWidth = sliderRef.current.clientWidth;
-            sliderRef.current.style.transform = `translateX(-${currentIndex * slideWidth}px)`;
-        }
-    };
-
-    useEffect(() => {
-        updateSliderPosition();
-    }, [currentIndex]);
-
-    const nextSlide = () => {
-        setCurrentIndex((prevIndex) => (prevIndex + 1) % slides.length);
-    };
-
-    const prevSlide = () => {
-        setCurrentIndex((prevIndex) => (prevIndex - 1 + slides.length) % slides.length);
-    };
-
-    // Using useInView hook to trigger animation for Trusted By section
-    const { ref: trustedByRef, inView: trustedByInView } = useInView({
-        triggerOnce: true,
-        threshold: 0.1,
-    });
-
-    // Using useInView hook to trigger animation for What We Offer section
-    const { ref: whatWeOfferRef, inView: whatWeOfferInView } = useInView({
-        triggerOnce: true,
-        threshold: 0.1,
-    });
-
-    return (
-        <div>
-            {/* Trusted By Section */}
-            <section
-                className={`trusted-by-section ${trustedByInView ? 'slide-in' : 'hidden'}`}
-                ref={trustedByRef}
-            >
-                <div className="trusted-by-title">
-                    <h2>— Trusted By</h2>
-                </div>
-                <div className="trusted-logos">
-                    <img className={`trusted-by-section ${trustedByInView ? 'slide-in' : 'hidden'}`} src={images.icon1} alt="Ufone Logo" />
-                    <img src={images.icon2} alt="Urban Point Logo" />
-                    <img src={images.icon3} alt="Ericsson Logo" />
-                    <img src={images.icon4} alt="Nokia Logo" />
-                    <img src={images.icon5} alt="Zong Logo" />
-                    <img src={images.icon6} alt="Huawei Logo" />
-                    <img src={images.icon7} alt="ZTE Logo" />
-                    <img src={images.icon8} alt="Huawei Logo" />
-                    <img src={images.icon9} alt="ZTE Logo" />
-                    <img src={images.icon10} alt="Zong Logo" />
-                    <img src={images.icon11} alt="Huawei Logo" />
-                    <img src={images.icon12} alt="ZTE Logo" />
-                    <img src={images.icon13} alt="Huawei Logo" />
-                    <img src={images.icon14} alt="ZTE Logo" />
-                </div>
-            </section>
-
-            {/* What We Offer Section */}
-            <section
-                className={`innovation-section ${whatWeOfferInView ? 'slide-in' : 'hidden'}`}
-                ref={whatWeOfferRef}
-            >
-                <div className="offer-text">
-                    <h5>Global Center of <span>INNOVATION</span></h5>
-                    <h5>— OUR SERVICES<h2>What We Offer</h2></h5>
-                    <p>
-                        Our services prioritize strategy and<br/>
-                        transparency as fundamental elements. Our<br/>
-                        objective is to enable you to expand your<br/>
-                        business by eliminating technological<br/>
-                        obstacles and providing comprehensive<br/>
-                        support throughout the project's lifecycle.
-                    </p>
-                    <a href="#" className="view-services"><h5>View All<span>Services &gt;</span></h5></a>
-                </div>
-                <div className="slider-wrapper">
-                    <div className='btnContainer'>
-                        <button className="control prev" onClick={prevSlide}>❮</button>
-                        <button className="control next" onClick={nextSlide}>❯</button>
-                    </div>
-                    <div className="slider-container">
-                        <div className="slider" ref={sliderRef}>
-                            {slides.map((slide, index) => (
-                                <div className="slide" key={index}>
-                                    <h3>{slide.title}</h3>
-                                    <p>{slide.description}</p>
-                                </div>
-                            ))}
-                        </div>
-                    </div>
-                </div>
-            </section>
-        </div>
+  const nextSlide = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % slides.length);
+  };
+  const prevSlide = () => {
+    setCurrentIndex(
+      (prevIndex) => (prevIndex - 1 + slides.length) % slides.length
     );
+  };
+  // Using useInView hook to trigger animation for Trusted By section
+  const { ref: trustedByRef, inView: trustedByInView } = useInView({
+    triggerOnce: true,
+    threshold: 0.1,
+  });
+
+  // Using useInView hook to trigger animation for What We Offer section
+  const { ref: whatWeOfferRef, inView: whatWeOfferInView } = useInView({
+    triggerOnce: true,
+    threshold: 0.1,
+  });
+
+  return (
+    <div>
+      {/* Trusted By Section */}
+      <section
+        className={`trusted-by-section ${
+          trustedByInView ? "slide-in" : "hidden"
+        }`}
+        ref={trustedByRef}
+      >
+        <div className="trusted-by-title">
+          <h2>— Trusted By</h2>
+        </div>
+        <div className="trusted-logos">
+          <img
+            className={`trusted-by-section ${
+              trustedByInView ? "slide-in" : "hidden"
+            }`}
+            src={images.icon1}
+            alt="Ufone Logo"
+          />
+          <img className={`trusted-by-section ${
+              trustedByInView ? "slide-in" : "hidden"
+            }`} 
+            src={images.icon2} alt="Urban Point Logo" />
+          <img className={`trusted-by-section ${
+              trustedByInView ? "slide-in" : "hidden"
+            }`} 
+            src={images.icon3} alt="Ericsson Logo" />
+          <img className={`trusted-by-section ${
+              trustedByInView ? "slide-in" : "hidden"
+            }`} 
+            src={images.icon4} alt="Nokia Logo" />
+          <img className={`trusted-by-section ${
+              trustedByInView ? "slide-in" : "hidden"
+            }`}
+            src={images.icon5} alt="Zong Logo" />
+          <img className={`trusted-by-section ${
+              trustedByInView ? "slide-in" : "hidden"
+            }`}
+            src={images.icon6} alt="Huawei Logo" />
+          <img className={`trusted-by-section ${
+              trustedByInView ? "slide-in" : "hidden"
+            }`} 
+            src={images.icon7} alt="ZTE Logo" />
+          <img className={`trusted-by-section ${
+              trustedByInView ? "slide-in" : "hidden"
+            }`} 
+            src={images.icon8} alt="Huawei Logo" />
+          <img className={`trusted-by-section ${
+              trustedByInView ? "slide-in" : "hidden"
+            }`}
+            src={images.icon9} alt="ZTE Logo" />
+          <img className={`trusted-by-section ${
+              trustedByInView ? "slide-in" : "hidden"
+            }`}
+            src={images.icon10} alt="Zong Logo" />
+          <img className={`trusted-by-section ${
+              trustedByInView ? "slide-in" : "hidden"
+            }`}
+            src={images.icon11} alt="Huawei Logo" />
+          <img className={`trusted-by-section ${
+              trustedByInView ? "slide-in" : "hidden"
+            }`}
+            src={images.icon12} alt="ZTE Logo" />
+          <img className={`trusted-by-section ${
+              trustedByInView ? "slide-in" : "hidden"
+            }`}src={images.icon13} alt="Huawei Logo" />
+          <img className={`trusted-by-section ${
+              trustedByInView ? "slide-in" : "hidden"
+            }`}src={images.icon14} alt="ZTE Logo" />
+        </div>
+
+      </section>
+
+      {/* What We Offer Section */}
+      <section
+        className={`innovation-section ${
+          whatWeOfferInView ? "slide-in" : "hidden"
+        }`}
+        ref={whatWeOfferRef}
+      >
+        <div className="offer-text">
+          <h5>
+            Global Center of <span>INNOVATION</span>
+          </h5>
+          <h5>
+            — OUR SERVICES<h2>What We Offer</h2>
+          </h5>
+          <p>
+            Our services prioritize strategy and
+            <br />
+            transparency as fundamental elements. Our
+            <br />
+            objective is to enable you to expand your
+            <br />
+            business by eliminating technological
+            <br />
+            obstacles and providing comprehensive
+            <br />
+            support throughout the project's lifecycle.
+          </p>
+          <a href="#" className="view-services">
+            <h5>
+              View All<span>Services &gt;</span>
+            </h5>
+          </a>
+        </div>
+        <div className="slider-wrapper">
+          <div className="btnContainer">
+            <button className="control prev" onClick={prevSlide}>
+              ❮
+            </button>
+            <button className="control next" onClick={nextSlide}>
+              ❯
+            </button>
+          </div>
+          <div className="slider-container">
+            <div className="slider" ref={sliderRef}>
+              {slides.map((slide, index) => (
+                <div className="slide" key={index}>
+                  <h3>{slide.title}</h3>
+                  <p>{slide.description}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default HeaderPage;
